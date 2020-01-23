a4a132e4f57ebdbdd4dbd344bcac485f08f3a4d8
Kathrin Eichinger <kathrin801@gmail.com>
2020-01-23 21:12:59 +0000

    removed experience section

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
-import Experience from './components/Experience'
 import Contact from './components/Contact'
 
 const App = () => {
@@ -27,7 +26,6 @@ const App = () => {
       </div>
     </div>
     <Projects />
-    <Experience />
     <Contact />
   </>)
   
