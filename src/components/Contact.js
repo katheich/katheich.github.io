34682f51682de18bb15f4685f143764865a7dc29
Kathrin Eichinger <kathrin801@gmail.com>
2020-01-24 14:06:12 +0000

    change link backslash

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,19 +5,18 @@ const Contact = () => {
   return (<section className="section" id="contact">
     <div className="container">
       <div className="title">
-        <span className="backslash">/</span>
-        <span className="backslash">/ </span>
+        <span className="backslash">// </span>
         Contact
       </div>
 
       <div className="level">
         <a className="level-item is-centered is-size-6" href="https://github.com/katheich" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-github-square"></i>
-          <div className="text">github.com/katheich</div>
+          <div className="text">github.com<span className="backslash">/</span>katheich</div>
         </a>
         <a className="level-item is-centered is-size-6" href="https://www.linkedin.com/in/kathrineichinger" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-linkedin"></i>
-          <div className="text">linkedin.com/in/kathrineichinger</div>
+          <div className="text">linkedin.com<span className="backslash">/</span>in<span className="backslash">/</span>kathrineichinger</div>
         </a>
       </div>
     </div>
