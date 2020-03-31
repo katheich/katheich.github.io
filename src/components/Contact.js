b920c4cd14d25a27dd1bd96b80e02011de4ba30a
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-31 16:58:07 +0100

    contacts section symbols, instagram added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,19 +4,16 @@ const Contact = () => {
 
   return (<section className="section" id="contact">
     <div className="container">
-      <div className="title">
-        <span className="backslash">// </span>
-        Contact
-      </div>
 
-      <div className="level">
-        <a className="level-item is-centered is-size-6" href="https://github.com/katheich" target="_blank" rel="noopener noreferrer">
+      <div className="contact-level">
+        <a className="item" href="https://github.com/katheich" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-github-square"></i>
-          <div className="text">github.com<span className="backslash">/</span>katheich</div>
         </a>
-        <a className="level-item is-centered is-size-6" href="https://www.linkedin.com/in/kathrineichinger" target="_blank" rel="noopener noreferrer">
+        <a className="item" href="https://www.linkedin.com/in/kathrineichinger" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-linkedin"></i>
-          <div className="text">linkedin.com<span className="backslash">/</span>in<span className="backslash">/</span>kathrineichinger</div>
+        </a>
+        <a className="item" href="https://www.instagram.com/katheich" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-instagram-square"></i>
         </a>
       </div>
     </div>
