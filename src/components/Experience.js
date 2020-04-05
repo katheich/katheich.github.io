fe7bb958824cb9bd129fb64f300148d3c1844196
Kathrin Eichinger <kathrin801@gmail.com>
2020-04-05 15:47:39 +0100

    experience info

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,6 +6,44 @@ import RBBLogo from './logos/RBBLogo'
 import BocconiLogo from './logos/BocconiLogo'
 import YorkLogo from './logos/YorkLogo'
 
+const info = {
+  pop: {
+    company: 'We Got POP',
+    dates: 'since April 2020',
+    title: [{ position: 'Associate Software Developer', dates: ''}],
+    location: 'London, United Kingdom',
+    link: 'https://www.wegotpop.com/'
+  },
+  ga: {
+    company: 'General Assembly',
+    dates: 'October 2019 - January 2020',
+    title: [{ position: 'Software Engineering Immersive Student', dates: ''}],
+    location: 'London, United Kingdom',
+    link: 'https://generalassemb.ly/education/software-engineering-immersive/london'
+  },
+  rbb: {
+    company: 'RBB Economics',
+    dates: 'July 2015 - June 2018',
+    title: [{ position: 'Senior Associate', dates: 'April-June \'18'}, { position: 'Associate', dates:'February \'16 - March \'18'}, { position: 'Intern', dates:'July-November \'15)'}],
+    location: 'Brussels, Belgium & London, United Kingdom',
+    link: 'http://www.rbbecon.com/'
+  },
+  bocconi: {
+    company: 'Bocconi University',
+    dates: '2013 - 2015',
+    title: [{ position: 'Master of Science in Economic and Social Sciences', dates: ''}],
+    location: 'Milan, Italy',
+    link: 'https://www.unibocconi.eu/wps/wcm/connect/Bocconi/SitoPubblico_EN/Navigation+Tree/Home/Programs/Master+of+Science/Economic+and+Social+Sciences/'
+  },
+  york: {
+    company: 'University of York',
+    dates: '2010 - 2013',
+    title: [{ position: 'Bachelor of Arts in Economics and Philosophy', dates: ''}],
+    location: 'York, United Kingdom',
+    link: 'https://www.york.ac.uk/study/undergraduate/courses/ba-economics-philosophy/'
+  }
+}
+
 const Experience = () => {
 
   const [hover, setHover] = useState(null)
@@ -24,7 +62,28 @@ const Experience = () => {
       <BocconiLogo size={50} id="bocconi" handleHover={handleHover}/>
       <YorkLogo size={50} id="york" handleHover={handleHover}/>
     </div>
-    {hover}
+    {hover && <div className="infos">
+      <div className="dates is-size-7">
+        {info[hover].dates}
+        <span className="backslash"> // </span>
+        {info[hover].location}
+      </div>
+      <div className="company is-size-5">
+        {info[hover].company}
+      </div>
+      
+
+      {info[hover].title.length > 1 ? info[hover].title.map((title, i) => {
+        return <div key={i} className="jobtitles is-size-6 has-text-weight-bold">
+          {title.position} <span className="has-text-weight-light is-size-7">{title.dates}</span>
+        </div>
+      }
+      ) : 
+      <div className="jobtitles is-size-6 has-text-weight-bold">{info[hover].title[0].position}</div>}
+  
+    </div>
+    }
+
   </div>)
 
 }
