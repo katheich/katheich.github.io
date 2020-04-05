322229b6bb62c6d005df6e59e8dfc2481b9e2140
Kathrin Eichinger <kathrin801@gmail.com>
2020-04-05 15:01:22 +0100

    experience logos

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-
+import POPLogo from './logos/POPLogo'
+import GALogo from './logos/GALogo'
+import RBBLogo from './logos/RBBLogo'
+import BocconiLogo from './logos/BocconiLogo'
+import YorkLogo from './logos/YorkLogo'
 
 const Experience = () => {
 
+  const [hover, setHover] = useState(null)
+
+  function handleHover(e) {
+    if (hover !== e.target.dataset.id) {
+      setHover(e.target.dataset.id)
+    }
+  }
+
   return (<div id="experience">
     <div className="logo-container">
-      
+      <POPLogo size={50} id="pop" handleHover={handleHover}/>
+      <GALogo size={50} id="ga" handleHover={handleHover}/>
+      <RBBLogo size={50} id="rbb" handleHover={handleHover}/>
+      <BocconiLogo size={50} id="bocconi" handleHover={handleHover}/>
+      <YorkLogo size={50} id="york" handleHover={handleHover}/>
     </div>
+    {hover}
   </div>)
 
 }
