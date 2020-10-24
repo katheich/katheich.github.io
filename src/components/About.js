49f78861cb3f8e28317157793bb031270e37c0ac
Kathrin Eichinger <kathrin801@gmail.com>
2020-10-24 13:23:50 +0100

    about typo

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ const About = () => {
       <div className="text is-size-6">
         I am a software developer based in London, UK.
         <br/><br/>
-        A keen learner with a background in economic consulting, I have experience in web development and data- processing, mainly in Python and JavaScript. Thriving in a collaborative environment and bringing with me an incessant need to understand “why”, I always look for opportunities to learn from and share my knowledge with others.        
+        A keen learner with a background in economic consulting, I have experience in web development and data-processing, mainly in Python and JavaScript. Thriving in a collaborative environment and bringing with me an incessant need to understand “why”, I always look for opportunities to learn from and share my knowledge with others.        
         <br/><br/>
         On this page you may find further information on my experience and skills, as well as explore my projects.
         <br/><br/>
