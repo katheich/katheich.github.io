ad2e16a32f2beddfa2d629ee1652afe3e3871544
Kathrin Eichinger <kathrin801@gmail.com>
2021-04-11 11:47:18 +0100

    refactor experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,43 +6,7 @@ import RBBLogo from './logos/RBBLogo'
 import BocconiLogo from './logos/BocconiLogo'
 import YorkLogo from './logos/YorkLogo'
 
-const info = {
-  pop: {
-    company: 'We Got POP',
-    dates: 'since April 2020',
-    title: [{ position: 'Software Developer', details: ''}],
-    location: 'London, United Kingdom',
-    link: 'https://www.wegotpop.com/'
-  },
-  ga: {
-    company: 'General Assembly',
-    dates: 'October 2019 - January 2020',
-    title: [{ position: 'Software Engineering Immersive', details: '12-weeks coding bootcamp' }],
-    location: 'London, United Kingdom',
-    link: 'https://generalassemb.ly/education/software-engineering-immersive/london'
-  },
-  rbb: {
-    company: 'RBB Economics',
-    dates: 'July 2015 - June 2018',
-    title: [{ position: 'Competition Economist', details: '' }],
-    location: 'Brussels, Belgium & London, United Kingdom',
-    link: 'http://www.rbbecon.com/'
-  },
-  bocconi: {
-    company: 'Bocconi University',
-    dates: '2013 - 2015',
-    title: [{ position: 'Economic and Social Sciences', details: 'Master of Science' }],
-    location: 'Milan, Italy',
-    link: 'https://www.unibocconi.eu/wps/wcm/connect/Bocconi/SitoPubblico_EN/Navigation+Tree/Home/Programs/Master+of+Science/Economic+and+Social+Sciences/'
-  },
-  york: {
-    company: 'University of York',
-    dates: '2010 - 2013',
-    title: [{ position: 'Economics and Philosophy', details: 'Bachelor of Arts' }],
-    location: 'York, United Kingdom',
-    link: 'https://www.york.ac.uk/study/undergraduate/courses/ba-economics-philosophy/'
-  }
-}
+import info from './data/experience_info.json' 
 
 const Experience = () => {
 
