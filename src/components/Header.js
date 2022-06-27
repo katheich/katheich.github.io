8bb0bb797468991ef7e76e7b05641db9c25ed0ab
Kathrin Eichinger <kathrin801@gmail.com>
2022-06-27 11:22:18 +0100

    horizontal line

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ function Header() {
   return (
     <header className={styles.header}>
       Kathrin Eichinger
+      <hr noshade className={styles.line}></hr>
       <div className={styles.subtitle}>Software Developer</div>
     </header>
   );
