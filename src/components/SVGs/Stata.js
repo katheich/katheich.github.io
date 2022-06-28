011129006ab0597724de3f47e4b08771ed365f9a
Kathrin Eichinger <kathrin801@gmail.com>
2022-06-28 10:59:06 +0100

    skills

---
+++ src/components/SVGs/Stata.js
@@ -0,0 +1,65 @@
+import styles from "../Sections.module.css";
+
+function StataLogo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -50 780 270"
+      height="21"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <g transform="translate(0,-792)" id="layer1">
+        <path
+          d="m 765.7,389.1 a 22.5,22.5 0 1 1 -45,0 22.5,22.5 0 1 1 45,0 z"
+          transform="matrix(0.7487,0,0,0.7487,214.1,541.5)"
+          id="path3000"
+          fill="none"
+          stroke="#000000"
+          fillOpacity="1"
+          stroke-width="3.0963"
+          strokeMiterlimit="4"
+          strokeDasharray="none"
+          className={styles.color}
+        />
+        <path
+          d="m 207.9,843.1 -14.6,38.5 -96.86,0 c -20.53,0 -20.76,27 0,27 l 56.66,0 c 72.2,-0.1 72.2,102.4 0,102.4 l -110.8,0 15.22,-37.6 97.08,0 c 20.9,0 21.4,-27.3 0,-27.3 l -55.99,0 c -73.16,0 -73.39,-103 0,-103 z"
+          id="path3782"
+          fill="#000000"
+          stroke="none"
+          className={styles.color}
+        />
+        <path
+          d="m 212,843.1 -14.6,38.5 46.1,0 0,129.4 37.9,0 0,-129.4 36.4,0 14.6,-38.5 z"
+          id="path3792"
+          fill="#000000"
+          stroke="none"
+          className={styles.color}
+        />
+        <path
+          d="m 335.4,843.1 -14.6,38.5 108.5,0 c 7.3,0 13.5,4.4 13.5,12.6 l 0,14.4 -85.9,0 c -66.3,0 -66.6,102.4 0,102.4 l 123.7,0 0,-117.4 c 0,-24.3 -20.4,-50.5 -53.3,-50.5 z m 23.5,103 83.9,0 0,27.3 -83.9,0 c -19.6,0 -19,-27.3 0,-27.3 z"
+          id="path3794"
+          fill="#000000"
+          stroke="none"
+          className={styles.color}
+        />
+        <path
+          d="m 619.7,843.1 -14.6,38.5 108.5,0 c 7.4,0 13.6,4.4 13.6,12.6 l 0,14.4 -85.9,0 c -66.3,0 -66.6,102.4 0,102.4 l 123.6,0 0,-117.4 c 0,-24.3 -20.4,-50.5 -53.2,-50.5 z m 23.5,103 84,0 0,27.3 -84,0 c -19.5,0 -19,-27.3 0,-27.3 z"
+          id="path3794-5"
+          fill="#000000"
+          stroke="none"
+          className={styles.color}
+        />
+        <path
+          d="m 495.6,843.1
+           -14.6,38.5 46.1,0 0,129.4 37.8,0 0,-129.4 36.4,0 14.7,-38.5 z"
+          id="path3792-3"
+          fill="#000000"
+          stroke="none"
+          className={styles.color}
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default StataLogo;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,140 @@
 import styles from "./Sections.module.css";
 
+import ArcGISLogo from "./SVGs/ArcGIS";
+import StataLogo from "./SVGs/Stata";
+
 function Skills() {
   return (
     <div className={styles.sectionContainer}>
       <div className={styles.sectionHeader}>Tech I've worked with</div>
-      <div className={styles.sectionText}>
-        <i className={`devicon-python-plain ${styles.skill}`}></i>
-        <i className={`devicon-javascript-plain ${styles.skill}`}></i>
-        <i className={`devicon-react-original ${styles.skill}`}></i>
-        <i className={`devicon-docker-plain ${styles.skill}`}></i>
-        <i className={`devicon-postgresql-plain ${styles.skill}`}></i>
-        <i className={`devicon-sqlalchemy-plain ${styles.skill}`}></i>
+      <div className={styles.skillsContainer}>
+        {/* Python */}
+        <div
+          className={`${`${styles.singleSkillContainer} ${styles.greenSkill}`} ${
+            styles.orangeSkill
+          }`}
+        >
+          <div className={styles.skillName}>Python</div>
+          <i className={`devicon-python-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* JavaScript */}
+        <div className={`${styles.singleSkillContainer} ${styles.orangeSkill}`}>
+          <div className={styles.skillName}>JavaScript</div>
+          <i className={`devicon-javascript-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* React */}
+        <div className={`${styles.singleSkillContainer} ${styles.blueSkill}`}>
+          <div className={styles.skillName}>React</div>
+          <i className={`devicon-react-original ${styles.skill}`}></i>
+        </div>
+
+        {/* TypeScript */}
+        <div className={`${styles.singleSkillContainer} ${styles.greenSkill}`}>
+          <div className={styles.skillName}>TypeScript</div>
+          <i className={`devicon-typescript-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* Docker */}
+        <div className={`${styles.singleSkillContainer} ${styles.orangeSkill}`}>
+          <div className={styles.skillName}>Docker</div>
+          <i className={`devicon-docker-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* PostgreSQL */}
+        <div className={`${styles.singleSkillContainer} ${styles.blueSkill}`}>
+          <div className={styles.skillName}>Postgre&shy;SQL</div>
+          <i className={`devicon-postgresql-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* AWS */}
+        <div className={`${styles.singleSkillContainer} ${styles.greenSkill}`}>
+          <div className={styles.skillName}>AWS</div>
+          <i className={`fa-brands fa-aws ${styles.skill}`}></i>
+        </div>
+
+        {/* Terraform */}
+        <div className={`${styles.singleSkillContainer} ${styles.orangeSkill}`}>
+          <div className={styles.skillName}>Terraform</div>
+          <i className={`devicon-terraform-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* Node */}
+        <div className={`${styles.singleSkillContainer} ${styles.blueSkill}`}>
+          <div className={styles.skillName}>Node.js</div>
+          <i className={`devicon-nodejs-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* Django */}
+        <div className={`${styles.singleSkillContainer} ${styles.greenSkill}`}>
+          <div className={styles.skillName}>Django</div>
+          <i className={`devicon-django-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* SASS */}
+        <div className={`${styles.singleSkillContainer} ${styles.orangeSkill}`}>
+          <div className={styles.skillName}>Sass</div>
+          <i className={`devicon-sass-original ${styles.skill}`}></i>
+        </div>
+
+        {/* Bootstrap */}
+        <div className={`${styles.singleSkillContainer} ${styles.blueSkill}`}>
+          <div className={styles.skillName}>Bootstrap</div>
+          <i className={`devicon-bootstrap-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* SQLAlchemy */}
+        <div className={`${styles.singleSkillContainer} ${styles.greenSkill}`}>
+          <div className={styles.skillName}>SQL&shy;Alchemy</div>
+          <i className={`devicon-sqlalchemy-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* SQLite */}
+        <div className={`${styles.singleSkillContainer} ${styles.orangeSkill}`}>
+          <div className={styles.skillName}>SQLite</div>
+          <i className={`devicon-sqlite-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* MongoDB */}
+        <div className={`${styles.singleSkillContainer} ${styles.blueSkill}`}>
+          <div className={styles.skillName}>MongoDB</div>
+          <i className={`devicon-mongodb-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* Jupyter */}
+        <div className={`${styles.singleSkillContainer} ${styles.greenSkill}`}>
+          <div className={styles.skillName}>Jupyter</div>
+          <i className={`devicon-jupyter-plain ${styles.skill}`}></i>
+        </div>
+
+        {/* Stata */}
+        <div className={`${styles.singleSkillContainer} ${styles.orangeSkill}`}>
+          <div className={styles.skillName}>Stata</div>
+          <figure className={`${styles.logoSVG} ${styles.skill}`}>
+            <StataLogo />
+          </figure>
+        </div>
+
+        {/* ArcGIS Pro */}
+        <div className={`${styles.singleSkillContainer} ${styles.blueSkill}`}>
+          <div className={styles.skillName}>ArcGIS Pro</div>
+          <figure className={`${styles.logoSVG} ${styles.skill}`}>
+            <ArcGISLogo />
+          </figure>
+        </div>
+
+        {/* LaTeX */}
+        <div className={`${styles.singleSkillContainer} ${styles.greenSkill}`}>
+          <div className={styles.skillName}>Latex</div>
+          <i className={`devicon-latex-original ${styles.skill}`}></i>
+        </div>
+
+        {/* Matlab */}
+        <div className={`${styles.singleSkillContainer} ${styles.orangeSkill}`}>
+          <div className={styles.skillName}>Matlab</div>
+          <i className={`devicon-matlab-plain ${styles.skill}`}></i>
+        </div>
       </div>
     </div>
   );
