d879060226e2f9ab7c1c611e9a2844aba6602c8b
Kathrin Eichinger <kathrin801@gmail.com>
2022-06-28 13:03:14 +0100

    mobile arrow

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Header scroll={scroll} />
       <About scroll={scroll} />
       <Skills />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import styles from "./Header.module.css";
 
-function Header() {
+function Header({ scroll }) {
   return (
     <header className={styles.header}>
-      Kathrin Eichinger
+      <div className={styles.filler}></div>
+      <div className={styles.title}>Kathrin Eichinger</div>
       <div className={styles.subtitle}>Software Developer</div>
+      <div className={styles.filler}>
+        <div className={styles.headerFooter}>
+          <i class={`fa-solid fa-angles-right ${styles.arrowRight}`}></i>
+        </div>
+      </div>
     </header>
   );
 }
