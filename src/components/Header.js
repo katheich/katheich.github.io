ceaebeca77e2a73a7bd17c983c23d77be08324a2
Kathrin Eichinger <kathrin801@gmail.com>
2023-03-31 18:10:11 +0100

    new header background

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,14 @@ import styles from "./Header.module.css";
 export function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.title}>Kathrin Eichinger</div>
-      <div className={styles.subtitle}>Software Developer</div>
+      <div className={styles.gradient}>
+        <div className={styles.overlay}>
+          <div className={styles.titlebox}>
+            <div className={styles.title}>Kathrin Eichinger</div>
+            <div className={styles.subtitle}>Software Developer</div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
