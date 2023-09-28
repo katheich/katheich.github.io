a0118bb9229ec7e0f9acfccc1b1848fcbd82ad7d
Kathrin Eichinger <kathrin801@gmail.com>
2023-09-28 09:51:26 +0100

    prettier

--- src/components/About.js
+++ src/components/About.js
@@ -10,36 +10,63 @@ export function About() {
       <div className={sectionStyles.sectionText}>
         <p>I am a software developer based in London.</p>
         <p>
-          My experience is mainly in full-stack web development, currently working as a 
-          Site Reliability Engineer (SRE) 
-          at <a href="https://careers.deliveroo.co.uk/" target="_blank">Deliveroo</a> after
-          a few years 
-          at <a href="https://www.ep.com/casting/" target="_blank">Entertainment Partners</a>. 
-          I also have a background in data analysis, with an education in economics and 
-          experience in a more client-facing role in economic consulting
-          at <a href="https://www.rbbecon.com/" target="_blank">RBB Economics</a>. 
+          My experience is mainly in full-stack web development, currently
+          working as a Site Reliability Engineer (SRE) at{" "}
+          <a href="https://careers.deliveroo.co.uk/" target="_blank">
+            Deliveroo
+          </a>{" "}
+          after a few years at{" "}
+          <a href="https://www.ep.com/casting/" target="_blank">
+            Entertainment Partners
+          </a>
+          . I also have a background in data analysis, with an education in
+          economics and experience in a more client-facing role in economic
+          consulting at{" "}
+          <a href="https://www.rbbecon.com/" target="_blank">
+            RBB Economics
+          </a>
+          .
         </p>
         <p>
-          My qualifications include a Master's degree in economics 
-          from <a href="https://www.unibocconi.eu/wps/wcm/connect/bocconi/sitopubblico_en/navigation+tree/home/programs/master+of+science/economic+and+social+sciences/" target="_blank">Bocconi University</a> and 
-          completing <a href="https://generalassemb.ly/education/software-engineering-immersive/london" target="_blank">General Assembly</a>'s 
-          Software Engineering Immersive bootcamp.
+          My qualifications include a Master's degree in economics from{" "}
+          <a
+            href="https://www.unibocconi.eu/wps/wcm/connect/bocconi/sitopubblico_en/navigation+tree/home/programs/master+of+science/economic+and+social+sciences/"
+            target="_blank"
+          >
+            Bocconi University
+          </a>{" "}
+          and completing{" "}
+          <a
+            href="https://generalassemb.ly/education/software-engineering-immersive/london"
+            target="_blank"
+          >
+            General Assembly
+          </a>
+          's Software Engineering Immersive bootcamp.
         </p>
         <p>
-          Thriving in a collaborative
-          environment and bringing with me an incessant need to understand
-          “why”, I always look for opportunities to learn from and share my
-          knowledge with others.
+          Thriving in a collaborative environment and bringing with me an
+          incessant need to understand “why”, I always look for opportunities to
+          learn from and share my knowledge with others.
         </p>
         <p>
-          Outside of work, I have volunteered 
-          with <a href="https://codefirstgirls.com/" target="_blank">Code First Girls</a> to 
-          teach the Introduction to
-          Python course and have run the London Landmarks Half Marathon
-          fundraising 
-          for <a href="https://www.wwf.org.uk/events/running" target="_blank">WWF</a>. I 
-          am also a cat foster carer 
-          at <a href="https://www.battersea.org.uk/support-us/fostering" target="_blank">Battersea Dogs & Cats Home</a>.
+          Outside of work, I have volunteered with{" "}
+          <a href="https://codefirstgirls.com/" target="_blank">
+            Code First Girls
+          </a>{" "}
+          to teach the Introduction to Python course and have run the London
+          Landmarks Half Marathon fundraising for{" "}
+          <a href="https://www.wwf.org.uk/events/running" target="_blank">
+            WWF
+          </a>
+          . I am also a cat foster carer at{" "}
+          <a
+            href="https://www.battersea.org.uk/support-us/fostering"
+            target="_blank"
+          >
+            Battersea Dogs & Cats Home
+          </a>
+          .
         </p>
         <p>Explore my full experience and projects on Linkedin and Github:</p>
         <a
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import styles from "./Header.module.css";
 export function Header() {
   return (
     <header className={styles.header}>
-        <div className={styles.overlay}>
-          <div className={styles.titlebox}>
-            <div className={styles.title}>Kathrin Eichinger</div>
-            <div className={styles.subtitle}>Software Developer</div>
-          </div>
+      <div className={styles.overlay}>
+        <div className={styles.titlebox}>
+          <div className={styles.title}>Kathrin Eichinger</div>
+          <div className={styles.subtitle}>Software Developer</div>
         </div>
+      </div>
     </header>
   );
 }
