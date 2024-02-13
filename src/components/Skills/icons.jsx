6eebb40a591d158cb350d038952f17d7544a0cc9
Kathrin Eichinger <kathrin801@gmail.com>
2024-02-13 20:14:51 +0000

    update skills

--- src/components/Skills/icons.jsx
+++ src/components/Skills/icons.jsx
@@ -1,15 +1,9 @@
-import { DataDogLogo } from "./SVGs/DataDog";
 import { ArcGISLogo } from "./SVGs/ArcGIS";
-import { StataLogo } from "./SVGs/Stata";
 
 import iconStyles from "./icons.module.css";
 
 const pythonIcon = (
   <i className={`devicon-python-plain ${iconStyles.skillIcon}`}></i>
-);
-
-const javascriptIcon = (
-  <i className={`devicon-javascript-plain ${iconStyles.skillIcon}`}></i>
 );
 
 const typescriptIcon = (
@@ -24,6 +18,9 @@ const golangIcon = (
   <i className={`devicon-go-plain ${iconStyles.skillIcon}`}></i>
 );
 
+const javaIcon = (
+  <i className={`devicon-java-plain ${iconStyles.skillIcon}`}></i>
+);
 const postgresIcon = (
   <i className={`devicon-postgresql-plain ${iconStyles.skillIcon}`}></i>
 );
@@ -32,16 +29,14 @@ const dockerIcon = (
   <i className={`devicon-docker-plain ${iconStyles.skillIcon}`}></i>
 );
 
-const awsIcon = <i className={`fa-brands fa-aws ${iconStyles.skillIcon}`}></i>;
+const awsIcon = (
+  <i
+    className={`devicon-amazonwebservices-plain-wordmark ${iconStyles.skillIcon}`}
+  ></i>
+);
 
 const terraformIcon = (
   <i className={`devicon-terraform-plain ${iconStyles.skillIcon}`}></i>
-);
-
-const datadogIcon = (
-  <figure className={`${iconStyles.logoSVG} ${iconStyles.skill}`}>
-    <DataDogLogo />
-  </figure>
 );
 
 const reactIcon = (
@@ -49,19 +44,23 @@ const reactIcon = (
 );
 
 const nodeIcon = (
-  <i className={`devicon-nodejs-plain ${iconStyles.skillIcon}`}></i>
+  <i className={`devicon-nodejs-plain-wordmark ${iconStyles.skillIcon}`}></i>
 );
 
 const djangoIcon = (
   <i className={`devicon-django-plain ${iconStyles.skillIcon}`}></i>
 );
 
-const bootstrapIcon = (
-  <i className={`devicon-bootstrap-plain ${iconStyles.skillIcon}`}></i>
-);
-
 const mongoIcon = (
   <i className={`devicon-mongodb-plain ${iconStyles.skillIcon}`}></i>
+);
+
+const dynamodbIcon = (
+  <i className={`devicon-dynamodb-plain ${iconStyles.skillIcon}`}></i>
+);
+
+const railsIcon = (
+  <i className={`devicon-rails-plain ${iconStyles.skillIcon}`}></i>
 );
 
 const figmaIcon = (
@@ -79,9 +78,7 @@ const arcgisIcon = (
 );
 
 const stataIcon = (
-  <figure className={`${iconStyles.logoSVG} ${iconStyles.skill}`}>
-    <StataLogo />
-  </figure>
+  <i className={`devicon-stata-original-wordmark ${iconStyles.skillIcon}`}></i>
 );
 
 const latexIcon = (
@@ -90,20 +87,20 @@ const latexIcon = (
 
 export const skillSet = [
   { id: "python", label: <>Python</>, icon: pythonIcon },
-  { id: "js", label: <>JavaScript</>, icon: javascriptIcon },
   { id: "ts", label: <>TypeScript</>, icon: typescriptIcon },
   { id: "ruby", label: <>Ruby</>, icon: rubyIcon },
   { id: "go", label: <>Go</>, icon: golangIcon },
-  { id: "postgres", label: <>Postgre&shy;SQL</>, icon: postgresIcon },
+  { id: "java", label: <>Java</>, icon: javaIcon },
   { id: "docker", label: <>Docker</>, icon: dockerIcon },
   { id: "aws", label: <>AWS</>, icon: awsIcon },
   { id: "tf", label: <>Terraform</>, icon: terraformIcon },
-  { id: "d", label: <>DataDog</>, icon: datadogIcon },
-  { id: "react", label: <>React</>, icon: reactIcon },
   { id: "node", label: <>Node.js</>, icon: nodeIcon },
-  { id: "django", label: <>Django</>, icon: djangoIcon },
-  { id: "bootstrap", label: <>Bootstrap</>, icon: bootstrapIcon },
+  { id: "react", label: <>React</>, icon: reactIcon },
+  { id: "postgres", label: <>Postgre&shy;SQL</>, icon: postgresIcon },
+  { id: "dynamo", label: <>Dynamo&shy;DB</>, icon: dynamodbIcon },
   { id: "mongo", label: <>MongoDB</>, icon: mongoIcon },
+  { id: "django", label: <>Django</>, icon: djangoIcon },
+  { id: "rails", label: <>Rails</>, icon: railsIcon },
   { id: "figma", label: <>Figma</>, icon: figmaIcon },
   { id: "jupyter", label: <>Jupyter</>, icon: jupyterIcon },
   { id: "arcgis", label: <>ArcGIS Pro</>, icon: arcgisIcon },
